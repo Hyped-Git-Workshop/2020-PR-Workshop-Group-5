5b43514ee324e16071731832d51528224af01e58
Neil Weidinger <neilweidingern@gmail.com>
2019-11-02 11:57:46 +0000

    initial commit

---
+++ pod.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include "pod.hpp"
+
+Pod::Pod()
+{}
+
+void Pod::setName(std::string name)
+{
+    name_ = name;
+}
+
+void Pod::setMaxSpeed(double speed)
+{
+    if (speed < 0) {
+        throw std::runtime_error("Speed is negative you doo doo");
+    }
+
+    max_speed_ = speed;
+}
+
+std::string Pod::getName()
+{
+    return name_;
+}
+
+double Pod::getMaxSpeed()
+{
+    return max_speed_;
+}
+
+int main(int argc, char* argv[])
+{
+    Pod my_pod {};
+
+    std::string name;
+    std::cout << "Enter name: ";
+    std::cin >> name;
+    my_pod.setName(name);
+
+    double speed;
+    std::cout << "Enter speed: ";
+    std::cin >> speed;
+    my_pod.setMaxSpeed(speed);
+
+    std::cout << "Pod name: " << my_pod.getName() << "\n";
+    std::cout << "Pod max speed: " << my_pod.getMaxSpeed() << "\n";
+}
---
+++ pod.hpp
@@ -0,0 +1,20 @@
+#ifndef HYPED_GIT_PR_WORKSHOP
+#define HYPED_GIT_PR_WORKSHOP
+
+#include <string>
+
+class Pod
+{
+    public:
+        Pod();
+        void setName(std::string name);
+        void setMaxSpeed(double speed);
+        std::string getName();
+        double getMaxSpeed();
+
+    private:
+        std::string name_;
+        double max_speed_;
+};
+
+#endif  // HYPED_GIT_PR_WORKSHOP
